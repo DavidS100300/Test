e42ca18ea6976d6086e99c5280213d6dec8f2abb
DavidS100300 <122007307+DavidS100300@users.noreply.github.com>
2023-01-05 14:53:31 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print('Hi! My name is David')
+    print('I am 22 years old')
+    print('I come from Indonesia')
+    print('I live in Q11 in HCMC')
+    print('Nice to meet you all!!')
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
